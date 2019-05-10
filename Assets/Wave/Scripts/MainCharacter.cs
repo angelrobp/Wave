93ef5ea74b455a98c8b99d0d50ef095844c21906
Ángel Robledillo Perea <angelrobp@gmail.com>
2019-05-10 18:43:55 +0200

    Añadido opciones en la escena Game

--- Assets/Wave/Scripts/MainCharacter.cs
+++ Assets/Wave/Scripts/MainCharacter.cs
@@ -8,15 +8,16 @@ public class MainCharacter : MonoBehaviour
     private MainCam mainCam = null;
     private BG_Scroll background = null;
 
+    private GameObject pausa;
     
-    public float positionX = 0f;
-    public float positionZ = 0f;
+    public float positionX, positionZ, positionXAntiguo, positionZAntiguo;
 
     public float moveSpeed = 10f;
 
     private float xAxis, zAxis;
-   
+    private Vector3 mousePosition;
 
+    private bool pausaActiva;
     private void Start()
     {
         // Eliminar gravedad
@@ -26,21 +27,66 @@ public class MainCharacter : MonoBehaviour
         background = objeto.GetComponent<BG_Scroll>();
         objeto = GameObject.Find("Main Camera");
         mainCam = objeto.GetComponent<MainCam>();
+
+        positionX = positionZ = positionXAntiguo = positionZAntiguo = 0f;
+        mousePosition = new Vector3(0f,0f,0f);
+
+        //Creación menu pausa
+        pausa = GameObject.Find("Menu Pausa");
+        pausaActiva = false;
+        pausa.SetActive(pausaActiva);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pausaActiva)
+            {
+                pausaActiva = false;
+
+            }
+            else
+            {
+                pausaActiva = true;
+            }
+            pausa.SetActive(pausaActiva);
+        }
+
+        mousePosition = Input.mousePosition;
+        print("Mouse: " + mousePosition);
+        //print("Personaje: "+ transform.position);
         //Movimiento personaje
         xAxis = Input.GetAxis("Horizontal");
         zAxis = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(xAxis, 0, zAxis);
-        positionX = xAxis*moveSpeed;
-        positionZ = zAxis*moveSpeed;
 
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
+        positionX = xAxis * moveSpeed;
+        positionZ = zAxis * moveSpeed;
+
+        
+        Vector3 movement;
+
+        if ((positionX < moveSpeed && positionX > -moveSpeed) && (positionZ < moveSpeed && positionZ > -moveSpeed))
+        {
+            positionX = positionXAntiguo;
+            positionZ = positionZAntiguo ;
+            movement = new Vector3(positionX, 0, positionZ);
+        }
+        else
+        {
+            movement = new Vector3(positionX, 0, positionZ);
+            positionXAntiguo = positionX;
+            positionZAntiguo = positionZ;
+        }
+        
+        
+
+        transform.Translate(movement * Time.deltaTime);
 
         //Movimiento camara para seguir al jugador
-        mainCam.move(movement * moveSpeed * Time.deltaTime);
+        mainCam.move(movement * Time.deltaTime);
+
+        
     }
 }
